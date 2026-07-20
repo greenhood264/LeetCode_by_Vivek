2626eea8bdde5f8f8a8e5d64ef774108fbcf78f6
Vivek Singh <greenhood265@gmail.com>
2026-07-20 19:44:15 +0530

    Time: 0 ms (100%), Space: 9 MB (54.89%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans="";
+        int cnt=0;
+        for( char c:s){
+            if(c==')') cnt--;
+            if(cnt>0) ans+=c;
+            if(c=='(') cnt++;
+        }
+        return ans;
+    }
+};
